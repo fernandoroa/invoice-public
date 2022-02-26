c4a02be87dddccbc44911b6db7d14a4668fa7236
Fernando Roa Ovalle <fernando@appsilon.com>
2022-02-26 08:12:03 -0300

    add other .json

--- app.R
+++ app.R
@@ -1,31 +1,66 @@
 library(shiny)
 library(dplyr)
-
+library(rjson)
 jsonL <- fromJSON(file = "input.json")
-originalSalary<-80
+jsonSalaryL <- fromJSON(file = "input_salary.json")
+
+originalSalary<-8000
 
 shinyApp(
   ui = fluidPage(
     titlePanel('Invoice Generator'),
-    sidebarLayout(
-      sidebarPanel(
-        checkboxInput("inter",HTML("<strong>Include Intermediary bank</strong>"),FALSE),
+    fluidPage(
+      fluidRow(
+        column(3
+        , wellPanel(
+        h4("other info"),
         textInput("invoiceNumber","Invoice Number","2022-"),
-        textInput("originalCurrency","original Currency","PLZ"),
-        numericInput("originalSalary","original Salary",originalSalary),
-        textInput("finalCurrency","final Currency","USD"),
         numericInput("exchangeRate","exchange Rate",4.1683),
         dateInput(inputId = 'start_date','Start Date: ', value = "2022-02-01"),
         dateInput('end_date','End Date: ', value = "2022-02-28"),
-        dateInput('inv_date','Invoice Date: ',value = "2022-02-28"),
-        downloadButton("report", "Generate invoice")
+        dateInput('inv_date','Invoice Date: ',value = "2022-02-28")
+        )
+        , wellPanel(
+        downloadButton("report", "Generate invoice in .pdf")
+        )
+        )
+        ,column(3
+        , wellPanel(
+          h4("from input_salary.json"),
+          textInput("originalCurrency","original Currency"
+                    ,jsonSalaryL$originalCurrency),
+          numericInput("originalSalary","original Salary"
+                       ,jsonSalaryL$originalSalary),
+          textInput("finalCurrency","final Currency",
+                    jsonSalaryL$finalCurrency),
+          checkboxInput("inter",HTML("<strong>Include Intermediary bank</strong>")
+                        ,jsonSalaryL$inter),
+          actionButton("modify_salary"
+                       , HTML("<strong>Update input_salary.json after changes!</strong>")
+                       )
+
+        )
   ),
-  mainPanel(
+  column(3,
     div(style="max-width:600px",
     uiOutput("jsonfields")
   )
-  ))),
-  server = function(input, output) {
+  ))
+  )
+  )
+  , server = function(input, output) {
+
+    observeEvent(input$modify_salary, {
+      inputList<-list()
+
+      inputList <- lapply(names(jsonSalaryL), function(x) {
+        input[[x]]
+      })
+
+      names(inputList) <- names(jsonSalaryL)
+      jsonData <- jsonlite::toJSON(x=inputList, pretty=TRUE)
+      write(jsonData, "input_salary.json")
+    })
 
     observeEvent(input$modify, {
       inputList<-list()
@@ -42,7 +77,9 @@ shinyApp(
     output$jsonfields<- renderUI({
       wellPanel(
         h3("This is the content of input.json"),
-        actionButton("modify", HTML("<strong>Update .json after changes!</strong>") ),
+        helpText("this box content must be saved before generating .pdf"),
+        actionButton("modify"
+                     , HTML("<strong>Save (input.json) required! after changes</strong>") ),
         br(),
         br(),
         lapply(seq_along(jsonL), function(x) {
