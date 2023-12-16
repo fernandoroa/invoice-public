706f8af71fbd1297356b0b4800a7b6efa279a8fc
Fernando Roa <froao@unal.edu.co>
2023-12-16 11:23:16 -0500

    refactor: module oneliner

--- app/modules/oneliner/__init__.R
+++ app/modules/oneliner/__init__.R
@@ -5,9 +5,9 @@ box::use(
 )
 
 box::use(
-  .. / utils / constants[...],
-  .. / logic / save_files[...],
-  .. / modules / single_oneliner
+  .. / .. / utils / constants[...],
+  .. / .. / logic / save_files[...],
+  .. / .. / modules / oneliner / single_oneliner
 )
 
 ui <- function(id) {
